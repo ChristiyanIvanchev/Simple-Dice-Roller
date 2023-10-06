b350b00dfe435ea671424e7f48ca2965367a46ae
ChristiyanIvanchev <147160553+ChristiyanIvanchev@users.noreply.github.com>
2023-10-06 13:31:17 +0100

    main.py

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ import random
 dice_numbers = list(range(2, 6+1))
 
 while True:
-    n = str(input('Input \'roll\' to throw the dice ot \'leave\' to stop the program!: '))
+    n = str(input('Input \'roll\' to throw the dice ot \'leave\' to stop the program!: ').lower())
     chosen_number = random.choice(dice_numbers)
     if n == 'roll':
         print(' ___')
         print('|', chosen_number, '|')
         print(' ---')
-        try_again = str(input('Do you want to try again? y/n: '))
+        try_again = str(input('Do you want to try again? y/n: ').lower())
         if try_again == 'y':
             continue
         elif try_again == 'n':
             print('Ok, bye')
+            break
+        else:
             break
     elif n == 'leave':
         print('Ok, bye')
